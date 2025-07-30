addbed1d66b938294afa78abb0d096b7179b6a9c
foxler5000 <redox.def@gmail.com>
2025-07-30 19:15:19 -0400

    change

--- mods/pepega/data/events/Change Character.hx
+++ mods/pepega/data/events/Change Character.hx
@@ -1,32 +1,27 @@
-var preloadedCharacters:Map<String, Character> = [];
-var preloadedIcons:Map<String, FlxSprite> = [];
+public var preloadedCharacters:Map<String, Character> = [];
+public var preloadedIcons:Map<String, FlxSprite> = [];
 
 function postCreate() {
-    for (icon in [iconP1, iconP2]) {
-        if (icon.animation == null || icon.animation.name == null) continue;
-        
-        preloadedIcons.set(icon.animation.name + (icon == iconP1 ? "-player" : ""), icon);
-    }
-    for (event in PlayState.SONG.events) {
-        if (event.name != "Change Character") continue;
+    for (event in PlayState.SONG.events)
+        if (event.name == "Change Character" && !preloadedCharacters.exists(event.params[1])) {
+            // Look for character that alreadly exists
+            var foundPreExisting:Bool = false;
+            for (strum in strumLines) 
+                for (char in strum.characters)
+                    if (char.curCharacter == event.params[1]) {
+                        preloadedCharacters.set(event.params[1], char);
+                        preloadedIcons.set(char.getIcon(), char == dad ? mistIconP2 : mistIconP1);
+                        foundPreExisting = true; break;
+                    }
+            if (foundPreExisting) continue;
 
-        var preExistingCharacter:Bool = false;
-        for (strum in strumLines)
-            for (char in strum.characters)
-                if (char.curCharacter == event.params[1]) {
-                    preloadedCharacters.set(event.params[1], char);
-                    preExistingCharacter = true;
-                    break;
-                }
-
-        var oldCharacter = strumLines.members[event.params[0]].characters[0];
-        // Create New Character
-        if (!preExistingCharacter) {
+            // Create New Character
+            var oldCharacter = strumLines.members[event.params[0]].characters[0];
             var newCharacter = new Character(oldCharacter.x, oldCharacter.y, event.params[1], oldCharacter.isPlayer);
             newCharacter.active = newCharacter.visible = false;
             newCharacter.drawComplex(FlxG.camera); // Push to GPU
             preloadedCharacters.set(event.params[1], newCharacter);
-            
+
             //Adjust Camera Offset to Accomedate Stage Offsets
             if(newCharacter.isGF) {
                 newCharacter.cameraOffset.x += stage.characterPoses["gf"].camxoffset;
@@ -38,20 +33,14 @@ function postCreate() {
                 newCharacter.cameraOffset.x += stage.characterPoses["dad"].camxoffset;
                 newCharacter.cameraOffset.y += stage.characterPoses["dad"].camyoffset;
             }
-        }
-        
-        // Create New Icon
-        var character:Character = preloadedCharacters.get(event.params[1]) ?? oldCharacter;
-        var iconName:String = character.getIcon() + (character.isPlayer ? "-player" : "");
-        if (preloadedIcons.exists(iconName)) continue;
 
-        var newIcon:HealthIcon = new HealthIcon(character.getIcon(), character.isPlayer);
-        newIcon.y = healthBar.y - (newIcon.height / 2);
-        newIcon.active = newIcon.visible = false;
-        newIcon.drawComplex(FlxG.camera); // Push to GPU
-        newIcon.cameras = [camHUD];
-        preloadedIcons.set(iconName, newIcon);
-    }
+            // Create New Icon
+            if (preloadedIcons.exists(newCharacter.getIcon())) continue;
+            var newIcon = createIcon(newCharacter);
+            newIcon.active = newIcon.visible = false;
+            newIcon.drawComplex(FlxG.camera); // Push to GPU
+            preloadedIcons.set(newCharacter.getIcon(), newIcon);
+        }
 }
 
 function onEvent(_) {
@@ -65,32 +54,29 @@ function onEvent(_) {
         insert(members.indexOf(oldCharacter), newCharacter);
         newCharacter.active = newCharacter.visible = true;
         remove(oldCharacter);
-
+	    //strumLines.members[params[0]].characters[0].curCharacter = params[1]; this makes it so fred doesn't switch back
         newCharacter.setPosition(oldCharacter.x, oldCharacter.y);
         newCharacter.playAnim(oldCharacter.animation.name);
         newCharacter.animation?.curAnim?.curFrame = oldCharacter.animation?.curAnim?.curFrame;
         strumLines.members[params[0]].characters[0] = newCharacter;
-        if (params[2]){
-        // Change Icon
-        var oldIcon = oldCharacter.isPlayer ? iconP1 : iconP2;
-        var newIcon = preloadedIcons.get(newCharacter.getIcon() + (oldCharacter.isPlayer ? "-player" : ""));
 
-        if (oldIcon.animation?.name == newIcon.animation?.name) return;
-     
-            insert(members.indexOf(oldIcon), newIcon);
-            newIcon.active = true;
-            newIcon.visible =  oldIcon.visible;
-           
-            remove(oldIcon);
-            if (oldCharacter.isPlayer) iconP1 = newIcon;
-            else iconP2 = newIcon;
-    
-       
-     
-         var leftColor:Int = dad != null && dad.visible && dad.iconColor != null && Options.colorHealthBar ? dad.iconColor : 0xFF000000;
-         var rightColor:Int = boyfriend != null && boyfriend.visible && boyfriend.iconColor != null && Options.colorHealthBar ? boyfriend.iconColor : 0xFF000000;
-         healthBar.createFilledBar(leftColor, rightColor);
-         healthBar.updateBar();
-        }
+        // Change Icon
+        var oldIcon = oldCharacter.isPlayer ? mistIconP1 : mistIconP2;
+        var newIcon = preloadedIcons.get(newCharacter.getIcon());
+
+        if (oldIcon == newIcon) return;
+        insert(members.indexOf(oldIcon), newIcon);
+        newIcon.active = newIcon.visible = true;
+        remove(oldIcon);
+        if (oldCharacter.isPlayer) mistIconP1 = newIcon;
+        else mistIconP2 = newIcon;
+        updateIcons(); 
+
+        var dadColor:Int = dad.iconColor != null && Options.colorHealthBar ? dad.iconColor : 0xFFFF0000;
+        var bfColor:Int = boyfriend.iconColor != null && Options.colorHealthBar ? boyfriend.iconColor : 0xFF66FF33;
+        var colors = [ dad, bfColor];
+        healthBar.createFilledBar((PlayState.opponentMode ? colors[1] : colors[0]), (PlayState.opponentMode ? colors[0] : colors[1]));
+        healthBar.updateBar();
+        //health -=0.00001;
     }
 }
